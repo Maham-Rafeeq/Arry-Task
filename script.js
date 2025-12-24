0da3a30936e4e99237cef6c9922110accca41b55
Maham Rafeeq <codewithmaham@gmail.com>
2025-12-24 18:26:20 +0500

    Add files via upload
    script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+//.push:arry k last ma element ko add kiya
+
+// let arry=["red","green","yello","pink"];
+// let push =arry.push("maham","rafeeq");
+// console.log(arry);
+
+// .pop:arry k last sy ak element remove kerwaya
+// let arry2 =["red","green","pink","purple","blue"]
+// let pop = arry2.pop();
+// console.log(arry2);
+
+
+// .unshift:arry k begning ma element add kerway
+// let arry3 =["apple","mango"]
+//  let unshift =arry3.unshift("banana","kiwi");
+// console.log(arry3)//;
+
+// .shift:arry ki begning ma sa ak element remove kr deta
+// let arry4=["chemistry","physics","math","english"]
+// let shift =arry4.shift();
+// console.log(arry4);
+
+// slice:ak new arry bana kr deta index number sy jo original arry ha us ma koi change nhi hota
+// let arry5 =["potato","Eggplant","ginger","garlic","tomato"]
+// let slice =arry5.slice(1,3);
+// console.log(slice);
+
+//.splice: delet or add ak sth kerta delete element k inedex sy add aesi string ma kr k element
+// let arry6 =["eagle","pigeon","Sparrow","Owl"]
+// let splice =arry6.splice(1,3, "duck","peacock");
+// console.log(arry6);
